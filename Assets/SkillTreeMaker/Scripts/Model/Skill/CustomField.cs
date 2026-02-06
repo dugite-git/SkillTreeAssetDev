af583dff200090f9a0b3cc1f0d0889dc54edb5b8
dugite-git <138967966+dugite-git@users.noreply.github.com>
2026-02-06 16:01:52 +0900

    Update CustomField.cs

--- Assets/SkillTreeMaker/Scripts/Model/Skill/CustomField.cs
+++ Assets/SkillTreeMaker/Scripts/Model/Skill/CustomField.cs
@@ -5,27 +5,21 @@ namespace SkillTreeMaker.Model.Skill
         private string name;
         private System.Type type;
         private object value = null;
-        private bool isUsed = false;
 
-        public CustomField(string name, System.Type type)
+        public CustomField(string name, System.Type type, object value = null)
         {
             this.name = name;
             this.type = type;
+            SetValue(value);
         }
 
         public void SetValue(object value)
         {
-            if (!isUsed) throw new System.Exception("Cannot set value of an unused attribute.");
             if (value.GetType() != type)
                 throw new System.ArgumentException(
                     $"Expected type '{type.Name}' but got '{value.GetType().Name}'.",
                     nameof(value));
             this.value = value;
-        }
-
-        public void SetIsUsed(bool isUsed)
-        {
-            this.isUsed = isUsed;
         }
     }
 }
